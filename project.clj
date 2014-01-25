893dc1ee7507e4dcf341b338482c1ee41bc0b0a2
Shane Kilkelly <shane@kilkelly.me>
2014-01-25 14:48:38 +0000

    update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject angular/lein-template "0.1.0"
-  :description "Clojure and AngularJS in perfect harmony."
-  :url "http://github.com/ShaneKilkelly/clojure-angular-seed"
+(defproject angular/lein-template "0.1.1"
+  :description "Clojure and AngularJS in perfect harmony. $ lein new angular <name>"
+  :url "http://github.com/ShaneKilkelly/lein-angular-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[leinjacker "0.2.0"]]
