47d63d85761cab54bc1ba0e70fee70916a58cb4d
Shane Kilkelly <shane@kilkelly.me>
2014-08-23 14:37:52 +0100

    update the lein dependencies

--- src/leiningen/new/angular/project.clj
+++ src/leiningen/new/angular/project.clj
@@ -3,17 +3,17 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [lib-noir "0.8.1"]
-                 [compojure "1.1.6"]
+                 [lib-noir "0.8.5"]
+                 [compojure "1.1.8"]
                  [ring-server "0.3.1"]
-                 [selmer "0.6.5"]
-                 [com.taoensso/timbre "3.1.6"]
+                 [selmer "0.6.9"]
+                 [com.taoensso/timbre "3.2.1"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "2.0.2"]
-                 [markdown-clj "0.9.40"]
-                 [liberator "0.11.0"]
-                 [clj-time "0.6.0"]
-                 [environ "0.4.0"]]
+                 [markdown-clj "0.9.47"]
+                 [liberator "0.12.1"]
+                 [clj-time "0.8.0"]
+                 [environ "1.0.0"]]
 
   :aot :all
   :repl-options {:init-ns {{ns-name}}.repl}
